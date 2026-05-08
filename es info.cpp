66f26f7bb75cbc56b0a4d70238599adce7a6b0e8
lorenzo800 <previtali.lorenzo.studente@itispaleocapa.it>
2026-05-08 10:34:33 +0200

    Add files via upload
    struct

---
+++ es info.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Info{
+string ClasseToponimo;	
+string DescrizioneToponimo	Numero;
+string Subalterno;
+int CAP;	
+int SezioneISTAT;	
+double Lat;	
+double Lon;	
+double Location;
+	
+};
+
+
+
+int main(int argc, char** argv) {
+		
+	
+	return 0;
+}
