95d6f04ce7f34353f603905507df0f30018665be
lorenzo800 <previtali.lorenzo.studente@itispaleocapa.it>
2026-05-08 10:54:26 +0200

    Add files via upload
    funzioni

--- es info.cpp
+++ es info.cpp
@@ -14,11 +14,26 @@ double Lon;
 double Location;
 	
 };
+void caricadati(info x[]; string a){
+	
+	//vedere se il file esiste se esiste
+ ifstream fileInput("testo.txt"); 
+    string linea;
 
+    if (fileInput.is_open()) {
+        while (fileInput>>linea){ 
+            cout << linea << '\n';
+        }
+        fileInput.close(); 
+    } else {
+        cout << "Impossibile aprire il file";
+    }
+    
+}
 
 
 int main(int argc, char** argv) {
 		
 	
 	return 0;
-}
+}   //prendere il file. prendee il contenuto e metterli qui
