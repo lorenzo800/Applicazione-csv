b94472bdb877f6948382dbecf7eb118a9c516820
lorenzo800 <previtali.lorenzo.studente@itispaleocapa.it>
2026-05-12 08:49:53 +0200

    Add files via upload
    struct

---
+++ info.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Info{
+string ClasseToponimo;	
+string DescrizioneToponimo;	
+string Numero;
+string Subalterno;
+string CAP;	
+string SezioneISTAT;			
+};
+
+
+
+struct Location{
+	double lat;
+	double lon;
+};
+
+          	
+void caricadati(info x[]; string a){
+	
+	//vedere se il file esiste se esiste
+ ifstream fileInput("testo.txt"); 
+    string linea;
+
+    if (fileInput.is_open()) {
+        while (fileInput>>linea){ 
+            cout << linea << '\n';
+        }
+        fileInput.close(); 
+    } else {
+        cout << "Impossibile aprire il file";
+    }
+    
+}
+
+ 
+int main(int argc, char** argv) {
+	
+	int x;
+	
+	cout<<"menù:"<<endl;
+	cout<<"1:aggiunge dati"<<endl;
+	cout<<"2:visualizza i dati"<<endl;
+	cout<<"inserire la funzione desiderata:"<<endl;
+	
+	cin>>x;
+	
+	switch(x){
+	case 1:
+	case 2:		
+	}
+	
+	
+	
+	
+	return 0;
+}   //prendere il file. prendee il contenuto e metterli qui
